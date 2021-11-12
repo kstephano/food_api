4d63843e159011b7f1419ed432c28a1ca962d667
Kelvin Stephano <kstephano@gmail.com>
2021-11-12 12:32:46 +0000

    created data

---
+++ data.js
@@ -0,0 +1,7 @@
+const foods = [
+    { id: 1, name: "Pizza", origin: "Italy" },
+    { id: 2, name: "Ramen", origin: "Japan" },
+    { id: 3, name: "Roast Dinner", origin: "England" }
+]
+
+module.exports = foods;
