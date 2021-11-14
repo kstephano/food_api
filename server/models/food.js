c1138704333573339f5172a9bf8c4424afae6308
Kelvin Stephano <kstephano@gmail.com>
2021-11-14 16:23:58 +0000

    all operations work

--- server/models/food.js
+++ server/models/food.js
@@ -12,25 +12,31 @@ class Food {
     }
 
     static findById(id) {
-        const food = null;
-        foodData.forEach(data => {
-            if (data.id === id) {
-                food = data
-            }
-        })
-        return food;
+        try {
+            const data = foodData.filter((food) => food.id === id)[0];
+            const food = new Food(data);
+            return food;
+        } catch (e) {
+            throw new Error("That does does not exist!");
+        }
     }
 
     static create(food) {
-        const id = foodData.length++;
+        const id = foodData.length + 1;
         const newFood = new Food({ id, ...food });
         foodData.push(newFood);
         return newFood;
     }
 
-    static delete() {
+    delete() {
         const food = foodData.filter((food) => food.id === this.id)[0];
         foodData.splice(foodData.indexOf(food), 1);
+    }
+
+    update(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.origin = data.origin;
     }
 }
 
