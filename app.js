b64dd950e978417081c5859a51e7972d4c6695d3
Kelvin Stephano <kstephano@gmail.com>
2021-11-14 16:21:17 +0000

    removed unnecessary api operations

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
+const port = 3000;
 const app = express();
-const port = 3004;
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).send('API home page!');
 });
 
 const foodRoutes = require('./server/controllers/foods');
 app.use('/foods', foodRoutes);
-
-app.post('/', (req, res) => {
-
-});
 
 app.listen(port, () => {
     console.log(`Food server listening at http://localhost:${port}`);
