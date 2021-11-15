0af676440f0a768af88eefe35770f96a22f7ddb9
Kelvin Stephano <kstephano@gmail.com>
2021-11-15 15:33:35 +0000

    fixed delete test, get all food tests if sorted

--- server/test/models.spec.js
+++ server/test/models.spec.js
@@ -11,21 +11,25 @@ describe ('Food model', () => {
         origin: 'Italy'
     };
 
-    it('should return all foods', () => {
+    it('should return all foods, sorted by id', () => {
         const foods = Food.allFood;
-        expect(foods).toEqual(foods);
+        expect(foods).toEqual([
+            { id: 1, name: "Pizza", origin: "Italy" },
+            { id: 2, name: "Ramen", origin: "Japan" },
+            { id: 3, name: "Roast Dinner", origin: "England" }
+        ]);
     });
 
     it('should delete a food', () => {
         const foods = Food.allFood;
         const foodToDelete = foods[0];
         foods[0].delete();
-
-        expect(foodsData).not.toContain(foodToDelete);
+        expect(Food.allFood).not.toContain(foodToDelete);
     });
 
     it('should update', () => {
         const foods = Food.allFood;
+
         const foodToUpdate = foods[0];
         foodToUpdate.update(testFood);
 
